ff54a261e7a21db69cd1b5066f2d7936ca5c6b4d
arao761 <basketballarao@gmail.com>
2024-05-27 22:45:47 -0400

    changed more stuff

--- app/chat/api/route.ts
+++ app/chat/api/route.ts
@@ -1,7 +1,7 @@
 import OpenAI from 'openai'
 import { OpenAIStream, StreamingTextResponse } from 'ai'
 
-const openai = new OpenAI(process.env.OPENAI_API_KEY)
+const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 export const runtime = 'edge'
 
 export async function POST(req: Request) {
